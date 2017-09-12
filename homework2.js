f6286bc1a8c41df474cf1ace3965a81cb89d6745
Henryabgaryan <henri.abgaryan@gmail.com>
2017-09-12 22:59:12 +0400

    hello

---
+++ homework2.js
@@ -0,0 +1,15 @@
+const fruit="apple";
+const year="2017";
+const write="pen"
+const my_name="Henry";
+const this_month="September"
+const my_age=17;
+console.log(fruit);
+console.log(my_age*10);
+console.log (my_age-year);
+console.log(my_name+this_month);
+console.log(year/0);
+console.log(fruit+write);
+console.log(0);
+
+ 
